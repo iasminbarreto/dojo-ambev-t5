a6636d1c50fce7a09e42f68ab06b33f70dc46ad0
Iasmin Victoria Teixeira Barreto <iasmin.barreto@ambevtech.com.br>
2022-10-07 17:30:59 -0300

    aula da semana 1

---
+++ cypress/e2e/3-ui/cadastro.cy.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+const faker = require('faker-br')
+describe ('US: Tela de cadastro', ()=> {
+
+    beforeEach(() => {
+        cy.visit('cadastrar')
+    });
+
+it('Cadastro com sucessos', () => {
+    let nome = faker.name.findName()
+    let email = faker.internet.email(nome)
+    cy.get('[data-test="register-name"] > .MuiInputBase-root > .MuiInputBase-input').type(nome)
+    cy.get('[data-test="register-email"]').type(email)
+    cy.get('[data-test="register-password"] > .MuiInputBase-root > .MuiInputBase-input').type('CalcinhaPreta')
+    cy.get('[data-test="register-password2"] > .MuiInputBase-root > .MuiInputBase-input').type('CalcinhaPreta{enter}')
+
+    cy.get('[data-test="dashboard-welcome"]').should('contain', 'Bem-vindo' + nome)
+
+});
+
+
+});
